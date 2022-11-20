47d9536410a3eb8aae3d82639ee354e5143100be
Divyansh Moonat <DivyanshM@edynamic.net>
2022-11-20 12:50:15 +0530

    State and lifecycle

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -3,21 +3,73 @@ import "./ProductCard.css";
 
 class ProductCard extends React.Component {
   constructor(props) {
+    console.log("I'm contructor, I'll be called only once upon intiliaziaion (Mounting Phase)")
     super();
+    this.state = {
+      qty: 0,
+    };
+    // this.qty = 0;
+  }
+
+  onQtyDecrease = () => {
+    // this.qty = this.qty - 1;
+    // this.forceUpdate();
+    // this.state.qty = this.state.qty - 1; // Wrong update
+    this.setState({ qty: this.state.qty - 1 });
+    // console.log("Quantity decreased");
+  };
+
+  onQtyIncrease = () => {
+    // this.qty = this.qty + 1;
+    // this.state.qty = this.state.qty + 1;// Wrong update
+    this.setState({ qty: this.state.qty + 1 });
+    // console.log("Quantity Increased");
+  };
+
+  componentDidMount() {
+    console.log("I'm componentDidMount and I'll be called only once in Mounting Phase")
+  }
+
+  componentDidUpdate() {
+    console.log("I'm componentDidUpdate and I'll be called on every state change (Updating Phase)");
+  }
+
+  componentWillUnmount() {
+    console.log("I'm componentWillUnmount, and I'll be called only when the component is removed fromt the screen i.e (Unmounting Phase)");
+  }
+
+  static getDerivedStateFromProps() {
+    console.log("I'm get derivedstateFromProps method");
+    return null;
+  }
+
+  shouldComponentUpdate() {
+    console.log("I'm shouldComonentUpdate method and I'll be called on every change (Updating Phase)");
+    return true;
+    // return false;
+  }
+
+  getSnapshotBeforeUpdate() {
+    console.log("I'm getSnapshotBeforeUpdate and I'll be called on every change (Updating Phase)");
+    return null;
   }
 
   render() {
+    console.log("I'm render method, I'll be triggered on every change")
+    const product = this.props.product;
     // console.log(this.props.product);
     return (
       <div className="product-card">
-        <img
-          src={this.props.product.image}
-          alt=""
-        />
+        <img src={product.image} alt="" />
         <div className="product-info">
-          <h5 className="title">{this.props.product.title}</h5>
-          <p className="price">Price Rs {this.props.product.price}</p>
-          <p className="category">{this.props.product.tags}</p>
+          <h5 className="title">{product.title}</h5>
+          <p className="price">Price Rs {product.price}</p>
+          <p className="category">{product.tags}</p>
+        </div>
+        <div className="cta">
+          <button onClick={this.onQtyDecrease}>-</button>
+          {this.state.qty}
+          <button onClick={this.onQtyIncrease}>+</button>
         </div>
       </div>
     );
--- src/container/Home/Home.js
+++ src/container/Home/Home.js
@@ -6,47 +6,64 @@ import "./Home.css";
 class Home extends React.Component {
   constructor() {
     super();
+    this.state = {
+      currentProductIndex: 0,
+      productDetails: [
+        {
+          image:
+            "https://rukminim1.flixcart.com/image/200/200/kx50gi80/pen/h/z/k/119766-flair-original-imag9nzubznagufg.jpeg?q=70",
+          title: "Box of Pencils",
+          price: 100,
+          tags: "Pens, Notebooks & More",
+        },
+        {
+          image:
+            "https://rukminim1.flixcart.com/image/200/200/jlph9jk0/cycle/h/f/k/skyper-26t-sskp26bk0001-16-hero-original-imaf8ru5xysfgtmx.jpeg?q=70",
+          title: "Cycle",
+          price: 10000,
+          tags: "Vehicles, bikes",
+        },
+        {
+          image:
+            "https://rukminim1.flixcart.com/image/200/200/krayqa80/book/k/b/b/guide-to-uttar-pradesh-up-nirikshak-platoon-commander-upsi-exam-original-imag54mwhwxgj6pj.jpeg?q=70",
+          title: "Book",
+          price: 200,
+          tags: "Study, books",
+        },
+        {
+          image:
+            "https://rukminim1.flixcart.com/image/200/200/light/h/9/h/imported-bicycle-rear-light-5-led-usb-rechargeable-waterproof-original-imaeq7hj3gppgcxz.jpeg?q=70",
+          title: "Cycle Lights",
+          price: 500,
+          tags: "Accessories, Vehicles",
+        },
+      ],
+    };
   }
 
+  onPreviousClick = () => {
+    this.setState({ currentProductIndex: this.state.currentProductIndex - 1 });
+  };
+
+  onNextClick = () => {
+    this.setState({ currentProductIndex: this.state.currentProductIndex + 1 });
+  };
+
   render() {
-    const productDetails = [
-      {
-        image:
-          "https://rukminim1.flixcart.com/image/200/200/kx50gi80/pen/h/z/k/119766-flair-original-imag9nzubznagufg.jpeg?q=70",
-        title: "Box of Pencils",
-        price: 100,
-        tags: "Pens, Notebooks & More",
-      },
-      {
-        image:
-          "https://rukminim1.flixcart.com/image/200/200/jlph9jk0/cycle/h/f/k/skyper-26t-sskp26bk0001-16-hero-original-imaf8ru5xysfgtmx.jpeg?q=70",
-        title: "Cycle",
-        price: 10000,
-        tags: "Vehicles, bikes",
-      },
-      {
-        image:
-          "https://rukminim1.flixcart.com/image/200/200/krayqa80/book/k/b/b/guide-to-uttar-pradesh-up-nirikshak-platoon-commander-upsi-exam-original-imag54mwhwxgj6pj.jpeg?q=70",
-        title: "Book",
-        price: 200,
-        tags: "Study, books",
-      },
-      {
-        image:
-          "https://rukminim1.flixcart.com/image/200/200/light/h/9/h/imported-bicycle-rear-light-5-led-usb-rechargeable-waterproof-original-imaeq7hj3gppgcxz.jpeg?q=70",
-        title: "Cycle Lights",
-        price: 500,
-        tags: "Accessories, Vehicles",
-      },
-    ];
+    const productDetails = this.state.productDetails;
     return (
       <main>
         <section>
           <h2>Stationary</h2>
+          <button onClick={this.onPreviousClick}>Previous</button>
+          <button onClick={this.onNextClick}>Next</button>
           <div className="products-container">
-            {productDetails.map((product) => {
+            <ProductCard
+              product={productDetails[this.state.currentProductIndex]}
+            />
+            {/* {productDetails.map((product) => {
               return <ProductCard key={product.title} product={product} />;
-            })}
+            })} */}
           </div>
         </section>
 
