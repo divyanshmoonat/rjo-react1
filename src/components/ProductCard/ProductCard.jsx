dcdf1902a7c03374c47eaa3fc9371a21a7f39bf9
Divyansh Moonat <DivyanshM@edynamic.net>
2022-11-26 16:50:16 +0530

    Conditional Rendering | OOO | Validations etc

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -3,11 +3,14 @@ import "./ProductCard.css";
 
 class ProductCard extends React.Component {
   constructor(props) {
-    console.log("I'm contructor, I'll be called only once upon intiliaziaion (Mounting Phase)")
+    // console.log("I'm contructor, I'll be called only once upon intiliaziaion (Mounting Phase)")
     super();
     this.state = {
       qty: 0,
+      availableQty: 5,
+      isOutOfStock: false,
     };
+    this.interval = null;
     // this.qty = 0;
   }
 
@@ -15,47 +18,96 @@ class ProductCard extends React.Component {
     // this.qty = this.qty - 1;
     // this.forceUpdate();
     // this.state.qty = this.state.qty - 1; // Wrong update
-    this.setState({ qty: this.state.qty - 1 });
+    this.setState(
+      { qty: this.state.qty - 1, availableQty: this.state.availableQty + 1 },
+      () => {
+        const isOutOfStock = this.state.availableQty === 0 ? true : false;
+        this.setState({ isOutOfStock: isOutOfStock });
+      }
+    );
+    this.props.onQtyUpdate("DEC");
     // console.log("Quantity decreased");
   };
 
   onQtyIncrease = () => {
     // this.qty = this.qty + 1;
     // this.state.qty = this.state.qty + 1;// Wrong update
-    this.setState({ qty: this.state.qty + 1 });
+
+    this.setState(
+      (prevState) => ({
+        qty: prevState.qty + 1,
+        availableQty: prevState.availableQty - 1,
+      }),
+      () => {
+        this.setState((prevState) => ({
+          isOutOfStock: prevState.availableQty === 0,
+        }));
+      }
+    );
+
+    // this.setState(
+    //   {
+    //     qty: this.state.qty + 1,
+    //     availableQty: this.state.availableQty - 1,
+    //   },
+    //   () => {
+    //     const isOutOfStock = this.state.availableQty === 0 ? true : false;
+    //     this.setState({ isOutOfStock });
+    //   }
+    // );
+    this.props.onQtyUpdate("INC");
     // console.log("Quantity Increased");
   };
 
   componentDidMount() {
-    console.log("I'm componentDidMount and I'll be called only once in Mounting Phase")
+    // this.interval = setInterval(() => {
+    //   if (this.state.qty < 6) {
+    //     this.setState({ qty: this.state.qty + 1 });
+    //   } else {
+    //     // console.log("Clear interval else part");
+    //     clearInterval(this.interval);
+    //   }
+    //   console.log("I'm timer", this.interval);
+    // }, 1000);
+    // setTimeout(() => {
+    //   this.setState({ qty: 5 });
+    // }, 3000);
+    // console.log(
+    //   "I'm componentDidMount and I'll be called only once in Mounting Phase"
+    // );
   }
 
-  componentDidUpdate() {
-    console.log("I'm componentDidUpdate and I'll be called on every state change (Updating Phase)");
-  }
+  // componentDidUpdate() {
+  //   if (this.state.qty !== 10) {
+  //     this.setState({ qty: 10 });
+  //   }
+  //   console.log(
+  //     "I'm componentDidUpdate and I'll be called on every state change (Updating Phase)"
+  //   );
+  // }
 
-  componentWillUnmount() {
-    console.log("I'm componentWillUnmount, and I'll be called only when the component is removed fromt the screen i.e (Unmounting Phase)");
-  }
+  // componentWillUnmount() {
+  //   console.log("I'm componentWillUnmount, and I'll be called only when the component is removed fromt the screen i.e (Unmounting Phase)");
+  // }
 
-  static getDerivedStateFromProps() {
-    console.log("I'm get derivedstateFromProps method");
-    return null;
-  }
+  // static getDerivedStateFromProps() {
+  //   console.log("I'm get derivedstateFromProps method");
+  //   return null;
+  // }
 
-  shouldComponentUpdate() {
-    console.log("I'm shouldComonentUpdate method and I'll be called on every change (Updating Phase)");
-    return true;
-    // return false;
-  }
+  // shouldComponentUpdate() {
+  //   console.log("I'm shouldComonentUpdate method and I'll be called on every change (Updating Phase)");
+  //   return true;
+  //   // return false;
+  // }
 
-  getSnapshotBeforeUpdate() {
-    console.log("I'm getSnapshotBeforeUpdate and I'll be called on every change (Updating Phase)");
-    return null;
-  }
+  // getSnapshotBeforeUpdate() {
+  //   console.log("I'm getSnapshotBeforeUpdate and I'll be called on every change (Updating Phase)");
+  //   return null;
+  // }
 
   render() {
-    console.log("I'm render method, I'll be triggered on every change")
+    // console.log("I'm render method, I'll be triggered on every change")
     const product = this.props.product;
     // console.log(this.props.product);
     return (
@@ -67,10 +119,27 @@ class ProductCard extends React.Component {
           <p className="category">{product.tags}</p>
         </div>
         <div className="cta">
-          <button onClick={this.onQtyDecrease}>-</button>
+          <button disabled={this.state.qty === 0} onClick={this.onQtyDecrease}>
+            -
+          </button>
           {this.state.qty}
-          <button onClick={this.onQtyIncrease}>+</button>
+          <button
+            disabled={this.state.isOutOfStock}
+            onClick={this.onQtyIncrease}
+          >
+            +
+          </button>
         </div>
+        {/* <span
+          style={{ display: this.state.isOutOfStock ? "inline-block" : "none" }}
+          className="ooo"
+        >
+          Out of Stock
+        </span> */}
+        {this.state.isOutOfStock && <span className="ooo">Out of Stock</span>}
+        {/* {this.state.isOutOfStock ? (
+          <span className="ooo">Out of Stock</span>
+        ) : null} */}
       </div>
     );
   }
--- src/container/Home/Home.js
+++ src/container/Home/Home.js
@@ -3,41 +3,43 @@ import ProductCard from "../../components/ProductCard/ProductCard";
 
 import "./Home.css";
 
+const productsList = [
+  {
+    image:
+      "https://rukminim1.flixcart.com/image/200/200/kx50gi80/pen/h/z/k/119766-flair-original-imag9nzubznagufg.jpeg?q=70",
+    title: "Box of Pencils",
+    price: 100,
+    tags: "Pens, Notebooks & More",
+  },
+  {
+    image:
+      "https://rukminim1.flixcart.com/image/200/200/jlph9jk0/cycle/h/f/k/skyper-26t-sskp26bk0001-16-hero-original-imaf8ru5xysfgtmx.jpeg?q=70",
+    title: "Cycle",
+    price: 10000,
+    tags: "Vehicles, bikes",
+  },
+  {
+    image:
+      "https://rukminim1.flixcart.com/image/200/200/krayqa80/book/k/b/b/guide-to-uttar-pradesh-up-nirikshak-platoon-commander-upsi-exam-original-imag54mwhwxgj6pj.jpeg?q=70",
+    title: "Book",
+    price: 200,
+    tags: "Study, books",
+  },
+  {
+    image:
+      "https://rukminim1.flixcart.com/image/200/200/light/h/9/h/imported-bicycle-rear-light-5-led-usb-rechargeable-waterproof-original-imaeq7hj3gppgcxz.jpeg?q=70",
+    title: "Cycle Lights",
+    price: 500,
+    tags: "Accessories, Vehicles",
+  },
+];
 class Home extends React.Component {
   constructor() {
     super();
     this.state = {
       currentProductIndex: 0,
-      productDetails: [
-        {
-          image:
-            "https://rukminim1.flixcart.com/image/200/200/kx50gi80/pen/h/z/k/119766-flair-original-imag9nzubznagufg.jpeg?q=70",
-          title: "Box of Pencils",
-          price: 100,
-          tags: "Pens, Notebooks & More",
-        },
-        {
-          image:
-            "https://rukminim1.flixcart.com/image/200/200/jlph9jk0/cycle/h/f/k/skyper-26t-sskp26bk0001-16-hero-original-imaf8ru5xysfgtmx.jpeg?q=70",
-          title: "Cycle",
-          price: 10000,
-          tags: "Vehicles, bikes",
-        },
-        {
-          image:
-            "https://rukminim1.flixcart.com/image/200/200/krayqa80/book/k/b/b/guide-to-uttar-pradesh-up-nirikshak-platoon-commander-upsi-exam-original-imag54mwhwxgj6pj.jpeg?q=70",
-          title: "Book",
-          price: 200,
-          tags: "Study, books",
-        },
-        {
-          image:
-            "https://rukminim1.flixcart.com/image/200/200/light/h/9/h/imported-bicycle-rear-light-5-led-usb-rechargeable-waterproof-original-imaeq7hj3gppgcxz.jpeg?q=70",
-          title: "Cycle Lights",
-          price: 500,
-          tags: "Accessories, Vehicles",
-        },
-      ],
+      productDetails: productsList,
+      cartItems: 0,
     };
   }
 
@@ -49,21 +51,54 @@ class Home extends React.Component {
     this.setState({ currentProductIndex: this.state.currentProductIndex + 1 });
   };
 
+  onQtyUpdate = (operation) => {
+    if (operation === "INC") {
+      this.setState({ cartItems: this.state.cartItems + 1 });
+    } else if (operation === "DEC") {
+      this.setState({ cartItems: this.state.cartItems - 1 });
+    }
+  };
+
+  onSearchValueChange = (e) => {
+    const searchKey = e.target.value;
+    const filteredList = productsList.filter((product) =>
+      product.title.toLowerCase().includes(searchKey.toLowerCase())
+    );
+    this.setState({ productDetails: filteredList });
+  };
+
   render() {
     const productDetails = this.state.productDetails;
     return (
       <main>
+        <section className="cart">
+          Items in Cart : {this.state.cartItems}
+        </section>
+        <div>
+          <input
+            onChange={this.onSearchValueChange}
+            placeholder="Search with item name.."
+            className="search-box"
+            type="text"
+          />
+        </div>
         <section>
           <h2>Stationary</h2>
           <button onClick={this.onPreviousClick}>Previous</button>
           <button onClick={this.onNextClick}>Next</button>
           <div className="products-container">
-            <ProductCard
+            {/* <ProductCard
               product={productDetails[this.state.currentProductIndex]}
-            />
-            {/* {productDetails.map((product) => {
-              return <ProductCard key={product.title} product={product} />;
-            })} */}
+            /> */}
+            {productDetails.map((product) => {
+              return (
+                <ProductCard
+                  key={product.title}
+                  product={product}
+                  onQtyUpdate={this.onQtyUpdate}
+                />
+              );
+            })}
           </div>
         </section>
 
